5dcf6f2afa6a23111363b98884fb03b2938529ec
Jake Luoma <luoma.jake@gmail.com>
2020-03-31 20:22:05 -0600

    Initial push

---
+++ mininet-mesh-topology.py
@@ -0,0 +1,79 @@
+#!/usr/bin/python
+
+from mininet.cli import CLI
+from mininet.node import Controller, OVSSwitch
+from mininet.net import Mininet
+from mininet.topo import Topo
+
+import os
+
+"""
+########
+Topologies
+########
+"""
+# Each host is connected to its own switch
+# The switches are connected in a chain
+# modified from https://github.com/mininet/mininet/wiki/Introduction-to-Mininet
+class ChainTopo(Topo):
+    def build(self, count=3):
+        # numSwitches used by Switches functions to enable each switch
+        # to have its own controller
+        global numSwitches
+        numSwitches = count
+
+        hosts = [self.addHost('h%d' % i) for i in range(1, count + 1)]
+        switches = [self.addSwitch('s%d' % i) for i in range(1, count + 1)]
+        for i in range(count):
+            self.addLink(hosts[i], switches[i])
+        for i in range(count-1):
+            self.addLink(switches[i], switches[i+1])
+
+topos = {'chain': (lambda: ChainTopo())}
+
+
+"""
+########
+Controllers
+########
+"""
+# A simple script to start my custom POX controller
+# TODO change name to custom script
+class POXBridge(Controller):
+    def start(self):
+        self.pox = '%s/pox/pox.py' % os.environ['HOME']
+        self.cmd(self.pox, 'forwarding.l2_learning &')
+
+    def stop( self ):
+        self.cmd('kill %' + self.pox)
+                                                                                                       
+controllers = {'poxbridge': POXBridge}
+
+
+"""
+########
+Switches
+########
+"""
+# modified from https://github.com/mininet/mininet/blob/master/examples/controllers.py
+# Code to enable each switch having its own controller
+defaultControllerPort = 6633
+numSwitches = 3
+controllers = [POXBridge('c%d' % i, port=defaultControllerPort + i) for i in range(0, numSwitches)]
+cmap = {}
+for i in range(0, numSwitches):
+    cmap['s%d' % i+1] = controllers[i]
+
+class MultiSwitch(OVSSwitch):
+    def start(self, controllers):
+        return OVSSwitch.start(self, [cmap[self.name]])
+
+
+if __name__ == '__main__':
+    net = Mininet(topo=ChainTopo(3), switch=MultiSwitch, build=False)
+    for c in controllers:
+        net.addController(c)
+    net.build()
+    net.start()
+    CLI(net)
+    net.stop()
